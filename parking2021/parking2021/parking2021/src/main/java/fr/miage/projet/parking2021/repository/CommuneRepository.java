972bfd1ad1359c3ebdce896bbfa72562c4f1a312
Yessoh bonny <63106083+YessohB@users.noreply.github.com>
2021-06-29 00:56:46 +0200

    Delete parking2021/parking2021/parking2021 directory

--- parking2021/parking2021/parking2021/src/main/java/fr/miage/projet/parking2021/repository/CommuneRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.miage.projet.parking2021.repository;
-
-import fr.miage.projet.parking2021.metier.Commune;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommuneRepository extends JpaRepository<Commune, Integer> {
-}
--- parking2021/parking2021/parking2021/src/main/java/fr/miage/projet/parking2021/repository/PlaceRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.miage.projet.parking2021.repository;
-
-import fr.miage.projet.parking2021.metier.Place;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaceRepository extends JpaRepository<Place, Integer> {
-}
